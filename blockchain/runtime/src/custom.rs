0d2c34e5a406ce4151e6752124577cb3488b18f1
NoCtrlZ <phantomofrotten@gmail.com>
2020-01-01 23:23:32 -0800

    describe for each components

--- blockchain/runtime/src/custom.rs
+++ blockchain/runtime/src/custom.rs
@@ -7,23 +7,33 @@ pub trait Trait: system::Trait {}
 
 #[derive(Encode, Decode, Default, Clone, PartialEq)]
 #[cfg_attr(feature = "std", derive(Debug))]
+
+// 提案
 pub struct Proposal <AccountId, Hash> {
+	// 提案者のEOA
 	proposer: AccountId,
+	// 提案のHash値
 	proposal_id: Hash,
 }
 
 decl_storage! {
     trait Store for Module<T: Trait> as Custom {
+		// 人口
 		Population: u64;
+		// DAO構成員のIDとEOA
 		People: map u64 => T::AccountId;
+		// EOAがDAOに登録されているか
 		IsRegistered: map T::AccountId => bool;
+		// 提案の構造体の現在のHash(Debug用)
 		PrevProposalId: T::Hash;
+		// 提案のHashと提案の構造体のマッピング
 		Proposals: map T::Hash => Proposal<T::AccountId, T::Hash>;
     }
 }
 
 decl_module! {
     pub struct Module<T: Trait> for enum Call where origin: T::Origin {
+		// DAOの構成員として登録する処理
         fn register(origin, account: T::AccountId) -> Result {
 			if !<IsRegistered<T>>::get(&account) {
 				let id = <Population<T>>::get();
@@ -34,6 +44,7 @@ decl_module! {
 			Ok(())
 		}
 
+		// 提案を作成する処理
 		fn propose(origin) -> Result {
 			let sender = ensure_signed(origin)?;
 			if <IsRegistered<T>>::get(&sender) {
